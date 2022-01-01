e989700f3dec21747fd24e829cdf75edd23ccac8
Defender007 <mydjangopythonprojects@gmail.com>
2022-01-01 23:46:06 +0100

    chore: modifies account admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
 # Register your models here.
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "username",
+        "email",
+        "date_joined",
+        "last_login",
+        "is_active",
+    )
+    list_display_links = ("first_name",
+        "last_name",
+        "email",)
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
