1d4c46d2e87520c49e9613838380cae2e5786340
Defender007 <mydjangopythonprojects@gmail.com>
2022-01-04 04:32:23 +0100

    feature: displays products by category

--- store/views.py
+++ store/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Product
+from category.models import Category
 
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
+def store(request, category_slug=None):
+    category = None
+    products = None
+    prod = None
+    
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.all().filter(category=category, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
     context = {
         'products': products,
         'product_count': product_count
