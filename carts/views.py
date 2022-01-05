45efbef389053f9fe6338ffd419d465bdd2bb041
Defender007 <mydjangopythonprojects@gmail.com>
2022-01-05 12:44:04 +0100

    feature: calculates Tax & Grand Total

--- carts/views.py
+++ carts/views.py
@@ -36,13 +36,18 @@ def cart(request, total=0, quantity=0, cart_items=None):
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
-            total += (cart_item.product.price * cart_item.quantity)
+            total += cart_item.product.price * cart_item.quantity
             quantity += cart_item.quantity
+        tax = (2 * total) / 100
+        grand_total = total + tax
     except ObjectDoesNotExist:
-        pass #just ignore
+        pass  # just ignore
     context = {
-        'total': total,
-        'quantity': quantity,
-        'cart_items': cart_items,
+        "total": total,
+        "quantity": quantity,
+        "cart_items": cart_items,
+        "tax": tax,
+        "grand_total": grand_total,
     }
+
     return render(request, "cart.html", context)
