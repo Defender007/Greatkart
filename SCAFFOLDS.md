04bad604f60b26469e21bcd2fc33922605d65d04
Defender007 <mydjangopythonprojects@gmail.com>
2022-04-10 15:36:18 +0100

    chore: makes some modifications...check the commits for details

--- SCAFFOLDS.md
+++ SCAFFOLDS.md
@@ -1,3 +1,8 @@
 installed pillow
 
 sets up store page
+
+<!-- admin user: -->
+email: superadmin@webapp.com
+username: python
+password: admin123
--- carts/views.py
+++ carts/views.py
@@ -49,15 +49,16 @@ def remove_cart_item(request, product_id):
     cart_item.delete()
     return redirect('cart')
 
-def cart(request, total=0, quantity=0, cart_items=None):
+def cart(request, total=0, quantity=0, cart_items=None, tax=0, grand_total=0): # the function signature was slightly modified by me...check the source code
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
-        for cart_item in cart_items:
-            total += cart_item.product.price * cart_item.quantity
-            quantity += cart_item.quantity
-        tax = (2 * total) / 100
-        grand_total = total + tax
+        if not cart_items: # I also run a check here for best practices sake...
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+            for cart_item in cart_items:
+                total += cart_item.product.price * cart_item.quantity
+                quantity += cart_item.quantity
+            tax = (2 * total) / 100
+            grand_total = total + tax
     except ObjectDoesNotExist:
         pass  # just ignore
     context = {
